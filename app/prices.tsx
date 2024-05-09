13100c68a9494f6d30615c1615ccb636e6093745
aniiaham <aniya_axm@icloud.com>
2024-05-08 22:14:28 -0400

    updated price history table

--- app/prices.tsx
+++ app/prices.tsx
@@ -79,21 +79,22 @@ export function TableDemo({ apt_number }: { apt_number: number }) {
         <TableRow className="text-blue-900 text-base font-medium font-sans">
           <TableHead className="w-[100px]">Date</TableHead>
           <TableHead>Location</TableHead>
+          <TableHead>Number</TableHead>
           <TableHead className="text-right">Amount</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {data.map((item) => (
-          <TableRow key={item.Id}>
-            <TableCell className="font-normal text-blue-900 text-sm font-sans">
+          <TableRow
+            key={item.Id}
+            className="text-right text-blue-900 text-sm font-normal font-sans"
+          >
+            <TableCell>
               {new Date(item.Date).toLocaleDateString("en-US")}
             </TableCell>
-            <TableCell className="text-blue-900 text-sm font-normal font-sans">
-              {item.Location}
-            </TableCell>
-            <TableCell className="text-right text-blue-900 text-sm font-normal font-sans">
-              ${item.Price}
-            </TableCell>
+            <TableCell>{item.Location}</TableCell>
+            <TableCell>{item.Number}</TableCell>
+            <TableCell>${item.Price}</TableCell>
           </TableRow>
         ))}
       </TableBody>
